9162e277960a49ff26947619c36d23cb518cc4c4
efy13 <efruze.ef@hotmail.com>
2025-04-03 08:03:42 -0700

    jv task

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+
+
+ let a = [1,2,3,4,5,];
+ let b=[4,5,6,7,8,9,];
+for(let i=0; i<a.length;i++){
+    for(let j=0;j<b.length;j++){
+        if(a[i]=== b[j]){
+            netice.push(a[i])
+        }
+    }
+}
+comsole.log(netice)
